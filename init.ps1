a4a974c8a57b588196c3f70cac1fdc9dd4d99efa
Stian Sivertsen <sivertsenstian@users.noreply.github.com>
2018-12-27 23:27:49 +0100

    initial scoop init

---
+++ init.ps1
@@ -0,0 +1,20 @@
+## install scoop.sh
+iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+set-executionpolicy remotesigned -scope currentuser
+
+# add buckets
+scoop bucket add extras
+scoop bucket add java
+
+# utils
+scoop install 7zip curl sudo git openssh coreutils ripgrep
+
+# programming languages
+scoop install nodejs openjdk
+
+# console theme
+scoop install concfg pshazz
+concfg import onedark
+
+# IDE
+scoop install emacs
