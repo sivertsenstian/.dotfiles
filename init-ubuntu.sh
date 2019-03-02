cf19e0f2b456167b4eb40dd5127440293529ed43
Stian Sivertsen <sivertsenstian@users.noreply.github.com>
2019-03-02 14:41:36 +0100

    Added nvm for node, and zsh

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -6,14 +6,23 @@ sudo add-apt-repository ppa:kelleyk/emacs;
 sudo apt -y update && sudo apt upgrade;
 
 # INSTALL CORE
-sudo apt -y install curl git;
+sudo apt -y install curl git zsh;
 
 # KEYBOARD LAYOUT
 sudo apt -y install x11-xkb-utils;
 
 # INSTALL DEPS
-sudo apt -y install nodejs emacs26;
+sudo apt -y install emacs26;
+
+# INSTALL NVM and NODE
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+source ~/.nvm/nvm.sh
+nvm install node
 
 # RIPGREP
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb;
 sudo dpkg -i ripgrep_0.10.0_amd64.deb;
+
+# INSTALL OH-MY-ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
